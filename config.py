0c4b4c26abfc6c4f387267547d6452f07203b299
TheatreGator <k.cleworth@outlook.com>
2026-08-04 13:25:17 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,35 @@
 # Edit these lists to control what the bot pulls from.
 
 RSS_FEEDS = [
-    "https://feeds.bbci.co.uk/news/rss.xml",
-    "https://www.theverge.com/rss/index.xml",
-    "https://feeds.npr.org/1001/rss.xml",
+    "https://www.broadway.com/feeds/buzz/latest",
+    "https://broadwaynews.com/rss",
+    "https://www.broadwayinchicago.com/feed",
+    "https://kendavenport.com/feed/",
+    "https://masterworksbroadway.com/category/blog/feed/",
+    "http://feeds.feedburner.com/BroadwayMe",
+    "https://www.westendtheatre.com/feed/news/",
+    "https://www.westendtheatre.com/feed/shows/",
+    "https://www.westendtheatre.com/feed/offers/",
+    "https://www.offwestend.com/feed/",
+    "https://stagesideuk.co.uk/feed/",
+    "https://www.thestage.co.uk/rss/news",
+    "https://www.whatsonstage.com/feed/",
+    "https://www.broadwayworld.com/feed/",
+    "https://playbill.com/feed/",
+    "https://www.theatermania.com/feed/",
+    "https://broadwaydirect.com/feed/",
 ]
 
 SUBREDDITS = [
-    "technology",
-    "worldnews",
-    "entertainment",
+    "Broadway",
+    "TheWestEnd",
+    "Theatre",
+    "musicals",
+    "musicaltheatre",
+    "techtheatre",
+    "offbroadway",
+    "Sondheim",
+    "acting",
+    "Playwriting",
 ]
+
